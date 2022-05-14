9194e98cdbb73fe447ae9f600dd4cee3964b1331
kate177 <kate177intro@bk.ru>
2022-05-14 16:06:47 +0300

    publick commit

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 const vueSrc = "./src";
 
 module.exports = defineConfig({
-   transpileDependencies: true,
-   configureWebpack: {
-      resolve: {
-         alias: {
-            "@": path.resolve(__dirname, vueSrc)
-         },
-      }
-   }
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, vueSrc),
+      },
+    },
+  },
+  publicPath: "/vue-star-wars/",
+});
